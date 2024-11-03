144e98bbad0fcec3be213a0c0ad24bb2b8ea44dd
Davit Grdzelishvili <166071024+davita101@users.noreply.github.com>
2024-11-04 02:23:51 +0400

    finished or alrady maedd projects

---
+++ level022/homework/app.js
@@ -0,0 +1,10 @@
+const myObj = {
+    title: prompt("Enter title: "),
+    author: prompt("Enter author: "),
+    years: prompt("Enter author: ")
+}
+
+//2 
+const circle = {
+    size: prompt("Enter size of circle: ")
+}
